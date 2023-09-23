d6b04e67e48f0338fd59d90d8406850ce7651185
Anchal Rathore <76997901+anchal0404@users.noreply.github.com>
2023-09-23 13:58:41 +0530

    23/09/2023

---
+++ sum.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+	int n1,n2,sum;
+	printf("Enter two number:");
+	scanf("%d %d",&n1,&n2);
+	sum=n1+n2;
+	printf("Sum :%d\n",sum);
+	return 0;
+}
