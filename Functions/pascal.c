71a32053aafaa4486631b398bb47e8be58cce6ad
Anchal Rathore <76997901+anchal0404@users.noreply.github.com>
2023-10-14 14:34:03 +0530

    14/10

--- Functions/pascal.c
+++ Functions/pascal.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-int factorial(int x)
+/*int factorial(int x)
 {
 	int fact=1;
 	for (int i = 2; i <= x; ++i)
@@ -15,7 +15,7 @@ int combinations(int n,int r)
 	int ncr=1;
 	ncr = factorial(n)/(factorial(r) * factorial(n-r));
 	return ncr;
-}
+}*/
 int main(int argc, char const *argv[])
 {
 	int n=1;
@@ -23,17 +23,21 @@ int main(int argc, char const *argv[])
 	scanf("%d",&n);
 	for (int i = 0; i <=n; ++i)
 	{
+		int first = 1;
+		//printf("%d",first);
 		for (int k = 0; k <= n-i ; ++k)
 		{
 			printf(" ");
 		}
 		for (int j = 0; j<=i; ++j)
 		{
-			printf("%d ",combinations(i,j));
+			printf("%d ",first);
+			first = first *(i-j)/(j+1);
+			
 		}
 		printf("\n");
 	}
 
-	
+
 	return 0;
 }
